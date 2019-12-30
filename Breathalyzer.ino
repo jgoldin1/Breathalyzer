7d42c2a5ea8b94ea4d0f462cfa4aaacea32392b0
jgoldin1 <59368924+jgoldin1@users.noreply.github.com>
2019-12-30 11:13:22 -0500

    Add files via upload

---
+++ Breathalyzer.ino
@@ -0,0 +1,48 @@
+#include <Wire.h>
+#include <Adafruit_AM2315.h>
+
+
+
+
+Adafruit_AM2315 am2315;
+
+const int JGPin = 13;
+const int AHPin = 12;
+const int alPin = 3;
+
+void setup() {
+  Serial.begin(57600);
+  
+  if (! am2315.begin()) {
+     Serial.println("Sensor not found, check wiring & pullups!");
+     while (1);
+  }
+  
+  pinMode(JGPin, INPUT);
+  pinMode(AHPin, INPUT);
+  
+  Serial.print("mils,Humidity,Temperature,A/J/N,Alcohol");
+}
+
+void loop() {
+  Serial.print(millis());
+  Serial.print(",");
+  
+  Serial.print(am2315.readHumidity()); Serial.print(",");
+  Serial.print(am2315.readTemperature());Serial.print(",");
+  
+  if(digitalRead(JGPin)==1)
+    Serial.print("J");
+  if(digitalRead(AHPin)==1)
+    Serial.print("A");
+  if( (digitalRead(AHPin)==0) && (digitalRead(JGPin)==0) )
+    Serial.print("N");
+  Serial.print(",");
+  
+  Serial.print(analogRead(alPin));
+  Serial.print("\n");
+  
+  delay(1000);
+  
+  //delay(1000);
+}
